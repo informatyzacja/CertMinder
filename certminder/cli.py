421be52602aed294f97bfc52a2025b35f54f5dd8
Szymon Datko <szymon.datko@pwr.edu.pl>
2022-07-14 19:22:32 +0200

    Parallelize checker and server
    This commit introduces multiprocessing to the cli module
    so both the checker and server modules can be run at the same
    time and immediately provide some results.

--- certminder/cli.py
+++ certminder/cli.py
@@ -1,9 +1,17 @@
 #!/usr/bin/env python3
 
+import multiprocessing
+
 from certminder import checker
 from certminder import server
 
 
 def main() -> None:
-    checker.main()
-    server.main()
+    checker_process = multiprocessing.Process(target=checker.main)
+    server_process = multiprocessing.Process(target=server.main)
+
+    checker_process.start()
+    server_process.start()
+
+    checker_process.join()
+    server_process.join()
